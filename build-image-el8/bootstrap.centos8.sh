4f61914d84edc0e6096ab32c2dc95fdbb42fd81a
Black Heat (undefined) <blackheatinc@gmail.com>
2020-03-09 10:23:06 +0700

    Minor fixes

--- build-image-el8/bootstrap.centos8.sh
+++ build-image-el8/bootstrap.centos8.sh
@@ -15,4 +15,4 @@ dnf install -y rpm-build spectool
 mkdir -p $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS,BUILDROOT}
 
 # Install build dependencies
-dnf install -y device-mapper-level git glib2-devel glibc-static gpgme-devel libassuan-devel libseccomp-devel pkgconf-pkg-config make golang go-compilers-golang-compiler container-selinux rpmdevtools libcap-devel systemd-devel python36 gettext libtool dos2unix
+dnf install -y device-mapper-devel git glib2-devel glibc-static gpgme-devel libassuan-devel libseccomp-devel pkgconf-pkg-config make golang go-compilers-golang-compiler container-selinux rpmdevtools libcap-devel systemd-devel python36 gettext libtool dos2unix
