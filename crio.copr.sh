5c2c4dc18c92215ddd51f0878e5a44423b498c81
Black Heat (undefined) <blackheatinc@gmail.com>
2020-03-10 13:36:52 +0700

    Added copr build scripts

---
+++ crio.copr.sh
@@ -0,0 +1,6 @@
+# Bootstrapping environment
+build-image-el8/bootstrap.centos8.sh
+
+# Build CRI-O
+cd crio-rpm
+./build.centos8.sh
---
+++ crun.copr.sh
@@ -0,0 +1,6 @@
+# Bootstrapping environment
+build-image-el8/bootstrap.centos8.sh
+
+# Build crun
+cd crun-rpm
+./build.centos8.sh
